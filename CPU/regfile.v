cce018e53bd5f45c1d6eec8ee518b1502a92a6bc
baki <yohtsubaki@yahoo.co.jp>
2020-12-27 15:47:46 +0900

    ugokanai register

--- CPU/regfile.v
+++ CPU/regfile.v
@@ -1,12 +1,33 @@
 module regfile (
   input clk,
   input reg_wire,
-  input [4:0] read_register1, read_register2, read_register3,
+  input [4:0] read_register1, read_register2, write_register,
   input [63:0] write_data,
   output [63:0] read_data1, read_data2
 );
 
+  reg [63:0] register [31:0];
+
   always @ ( posedge clk ) begin
-    
+      if(reg_wire)
+        register[write_register] <= write_data;
   end
+
+  /*
+  if(read_register1 != 0) begin
+    assign read_data1 = register[read_register1];
+  end else begin
+    assign read_data1 = 0;
+  end
+
+  if(read_register2 != 0) begin
+    assign read_data2 = register[read_register2];
+  end else begin
+    assign read_data2 = 0;
+  end
+  */
+
+  assign read_data1 = (read_register1 != 0) ? register[read_register1] : 0;
+  assign read_data2 = (read_register2 != 0) ? register[read_register2] : 0;
+
 endmodule //regfile
--- CPU/regfile_test.v
+++ CPU/regfile_test.v
@@ -1,13 +1,46 @@
-`timescale 1ns / 1ps
 `include "regfile.v"
 
 module testbench ();
 
+  reg clk;
+  reg reg_wire;
+  reg [4:0] read_register1, read_register2, write_register;
+  reg [63:0] write_data;
+  wire [63:0] read_data1, read_data2;
 
+  regfile regriletest(clk, reg_file, read_register1, read_register2,
+                      write_register, write_data, read_data1, read_data2);
 
   initial begin
     $dumpfile("regfile_test.vcd");
     $dumpvars(0, regriletest);
+    $monitor("%h, %h, %h, %h", regriletest.register[1],regriletest.register[2],
+            regriletest.read_data1, regriletest.read_data2);
+    //$display("register[1]   register[2]   read_data1   read_data2");
+
+    read_register1 <= 1;  read_register2 <= 2;  write_register <= 1;  write_data <= 5;
+    reg_wire <= 1;  #10;
+    reg_wire <= 0;  #10;
+
+    read_register1 <= 1;  read_register2 <= 2;  write_register <= 2;  write_data <= 6;
+    reg_wire <= 1;  #10;
+    reg_wire <= 0;  #10;
+
+    read_register1 <= 1;  read_register2 <= 2;  write_register <= 1;  write_data <= 8;
+    reg_wire <= 1;  #10;
+    reg_wire <= 0;  #10;
+
+    read_register1 <= 0;  read_register2 <= 2;  write_register <= 0;  write_data <= 8;
+    reg_wire <= 1;  #10;
+    reg_wire <= 0;  #10;
+
+    #200;
+    $finish;
+
+  end
+
+  always begin
+    clk <= 1; #5; clk <= 0; #5;
   end
 
 endmodule //testbench
